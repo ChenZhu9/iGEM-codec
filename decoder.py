ae891b561b20025fbad1cfac76a2b3a261c0a3e5
JimmyZhu1999 <56773218+JimmyZhu1999@users.noreply.github.com>
2019-10-22 03:36:14 +0800

    codec

--- decoder.py
+++ decoder.py
@@ -1,26 +1,28 @@
 from decode import *
 import xlrd
 data = xlrd.open_workbook("data.xls")
-table = data.sheets()[1]          #通过索引顺序获取
+table = data.sheets()[0]          #通过索引顺序获取
 nrows = table.nrows  #获取该sheet中的有效行数
 def Index():#Find the positon of data
-    tmp=[]
-    j=0
+    tmp1=[]
+    tmp2=[]
     for i in range(nrows):
         if(table.cell_value(i,0)=='A'):
-            tmp.append(i)
-            j=j+1
-    return tmp
+            if(int(table.cell_value(i,1))>=2):
+                tmp1.append(i)#flouresence
+            else:
+                tmp2.append(i)#od
+    return tmp1,tmp2
 
-index=Index()  
+flourindex,odindex=Index()  
 msg=[]
 def read(n):
     sum0=0
     tmp=[]
     data=""
     lenbyte=0
-    len0=index[n]-index[n-2]
-    for i in range (index[n-2],index[n-2]+8):
+    len0=odindex[n]-flourindex[n]
+    for i in range (flourindex[n],flourindex[n]+8):
         for j in range(1,13):
             gfp=float(table.cell_value(i,j))/float(table.cell_value(i+len0,j))
             tmp.append(gfp)
@@ -29,23 +31,23 @@ def read(n):
     for i in range (0,96):
         lenbyte=lenbyte+1
         if tmp[i]>=avg:
-            tmp[i]=0
-        else:
             tmp[i]=1
+        else:
+            tmp[i]=0
         data=data+str(tmp[i])
         if lenbyte==8:
             msg.append(int(data,2))
             data=""
             lenbyte=0
+for i in range(0,len(odindex)):
+    read(i)
 
-read(2)
-read(3)
-print (msg)
 nsym=4
 synd = rs_calc_syndromes(msg, nsym)
 err_loc = rs_find_error_locator(synd, nsym)
 pos = rs_find_errors(err_loc[::-1], len(msg)) # find the errors locations
-print(pos)
+print("Error site:",pos)
 msg = rs_correct_errata(msg, synd, pos)
 for i in range (len(msg)-nsym):
     print(chr(msg[i]), end='')
+
